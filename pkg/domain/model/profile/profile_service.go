e9c926201499f9141aef83e9bb76cb1680aac226
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2024-06-09 16:26:40 +0900

    Add: profileモデルを追加

---
+++ pkg/domain/model/profile/profile_service.go
@@ -0,0 +1,59 @@
+//go:generate mockgen -source=./profile_service.go -destination=./profile_service_mock.gen.go -package=profile
+package profile
+
+import (
+	"context"
+
+	"gorm.io/gorm"
+
+	"github.com/game-core/gc-server/internal/errors"
+	"github.com/game-core/gc-server/pkg/domain/model/profile/userProfile"
+)
+
+type ProfileService interface {
+	Get(ctx context.Context, req *ProfileGetRequest) (*ProfileGetResponse, error)
+	Create(ctx context.Context, tx *gorm.DB, req *ProfileCreateRequest) (*ProfileCreateResponse, error)
+	Update(ctx context.Context, tx *gorm.DB, req *ProfileUpdateRequest) (*ProfileUpdateResponse, error)
+}
+
+type profileService struct {
+	userProfileMysqlRepository userProfile.UserProfileMysqlRepository
+}
+
+func NewProfileService(
+	userProfileMysqlRepository userProfile.UserProfileMysqlRepository,
+) ProfileService {
+	return &profileService{
+		userProfileMysqlRepository: userProfileMysqlRepository,
+	}
+}
+
+// Get プロフィールを取得する
+func (s *profileService) Get(ctx context.Context, req *ProfileGetRequest) (*ProfileGetResponse, error) {
+	userProfileModel, err := s.userProfileMysqlRepository.Find(ctx, req.UserId)
+	if err != nil {
+		return nil, errors.NewMethodError("s.userProfileMysqlRepository.Find", err)
+	}
+
+	return SetProfileGetResponse(userProfileModel), nil
+}
+
+// Create プロフィールを作成する
+func (s *profileService) Create(ctx context.Context, tx *gorm.DB, req *ProfileCreateRequest) (*ProfileCreateResponse, error) {
+	userProfileModel, err := s.userProfileMysqlRepository.Create(ctx, tx, userProfile.SetUserProfile(req.UserId, req.Name, req.Content))
+	if err != nil {
+		return nil, errors.NewMethodError("s.userProfileMysqlRepository.Create", err)
+	}
+
+	return SetProfileCreateResponse(userProfileModel), nil
+}
+
+// Update プロフィールを更新する
+func (s *profileService) Update(ctx context.Context, tx *gorm.DB, req *ProfileUpdateRequest) (*ProfileUpdateResponse, error) {
+	userProfileModel, err := s.userProfileMysqlRepository.Update(ctx, tx, userProfile.SetUserProfile(req.UserId, req.Name, req.Content))
+	if err != nil {
+		return nil, errors.NewMethodError("s.userProfileMysqlRepository.Update", err)
+	}
+
+	return SetProfileUpdateResponse(userProfileModel), nil
+}
---
+++ pkg/infrastructure/mysql/user/userProfile/user_profile_dao.gen.go
@@ -0,0 +1,232 @@
+// Package userProfile ユーザープロフィール
+package userProfile
+
+import (
+	"context"
+
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+
+	"github.com/game-core/gc-server/config/database"
+	"github.com/game-core/gc-server/internal/errors"
+	"github.com/game-core/gc-server/internal/keys"
+	"github.com/game-core/gc-server/pkg/domain/model/profile/userProfile"
+)
+
+type userProfileDao struct {
+	ShardMysqlConn *database.ShardMysqlConn
+}
+
+func NewUserProfileDao(conn *database.MysqlHandler) userProfile.UserProfileMysqlRepository {
+	return &userProfileDao{
+		ShardMysqlConn: conn.User,
+	}
+}
+
+func (s *userProfileDao) Find(ctx context.Context, userId string) (*userProfile.UserProfile, error) {
+	t := NewUserProfile()
+	res := s.ShardMysqlConn.Shards[keys.GetShardKeyByUserId(userId)].ReadMysqlConn.WithContext(ctx).Where("user_id = ?", userId).Find(t)
+	if err := res.Error; err != nil {
+		return nil, err
+	}
+	if res.RowsAffected == 0 {
+		return nil, errors.NewError("record does not exist")
+	}
+
+	return userProfile.SetUserProfile(t.UserId, t.Name, t.Content), nil
+}
+
+func (s *userProfileDao) FindOrNil(ctx context.Context, userId string) (*userProfile.UserProfile, error) {
+	t := NewUserProfile()
+	res := s.ShardMysqlConn.Shards[keys.GetShardKeyByUserId(userId)].ReadMysqlConn.WithContext(ctx).Where("user_id = ?", userId).Find(t)
+	if err := res.Error; err != nil {
+		return nil, err
+	}
+	if res.RowsAffected == 0 {
+		return nil, nil
+	}
+
+	return userProfile.SetUserProfile(t.UserId, t.Name, t.Content), nil
+}
+
+func (s *userProfileDao) FindList(ctx context.Context, userId string) (userProfile.UserProfiles, error) {
+	ts := NewUserProfiles()
+	res := s.ShardMysqlConn.Shards[keys.GetShardKeyByUserId(userId)].ReadMysqlConn.WithContext(ctx).Where("user_id = ?", userId).Find(&ts)
+	if err := res.Error; err != nil {
+		return nil, err
+	}
+
+	ms := userProfile.NewUserProfiles()
+	for _, t := range ts {
+		ms = append(ms, userProfile.SetUserProfile(t.UserId, t.Name, t.Content))
+	}
+
+	return ms, nil
+}
+
+func (s *userProfileDao) Create(ctx context.Context, tx *gorm.DB, m *userProfile.UserProfile) (*userProfile.UserProfile, error) {
+	var conn *gorm.DB
+	if tx != nil {
+		conn = tx
+	} else {
+		conn = s.ShardMysqlConn.Shards[keys.GetShardKeyByUserId(m.UserId)].WriteMysqlConn
+	}
+
+	t := &UserProfile{
+		UserId:  m.UserId,
+		Name:    m.Name,
+		Content: m.Content,
+	}
+	res := conn.Model(NewUserProfile()).WithContext(ctx).Create(t)
+	if err := res.Error; err != nil {
+		return nil, err
+	}
+
+	return userProfile.SetUserProfile(t.UserId, t.Name, t.Content), nil
+}
+
+func (s *userProfileDao) CreateList(ctx context.Context, tx *gorm.DB, ms userProfile.UserProfiles) (userProfile.UserProfiles, error) {
+	if len(ms) <= 0 {
+		return ms, nil
+	}
+
+	fms := ms[0]
+	for _, m := range ms {
+		if m.UserId != fms.UserId {
+			return nil, errors.NewError("userId is invalid")
+		}
+	}
+
+	var conn *gorm.DB
+	if tx != nil {
+		conn = tx
+	} else {
+		conn = s.ShardMysqlConn.Shards[keys.GetShardKeyByUserId(fms.UserId)].WriteMysqlConn
+	}
+
+	ts := NewUserProfiles()
+	for _, m := range ms {
+		t := &UserProfile{
+			UserId:  m.UserId,
+			Name:    m.Name,
+			Content: m.Content,
+		}
+		ts = append(ts, t)
+	}
+
+	res := conn.Model(NewUserProfile()).WithContext(ctx).Create(ts)
+	if err := res.Error; err != nil {
+		return nil, err
+	}
+
+	return ms, nil
+}
+
+func (s *userProfileDao) Update(ctx context.Context, tx *gorm.DB, m *userProfile.UserProfile) (*userProfile.UserProfile, error) {
+	var conn *gorm.DB
+	if tx != nil {
+		conn = tx
+	} else {
+		conn = s.ShardMysqlConn.Shards[keys.GetShardKeyByUserId(m.UserId)].WriteMysqlConn
+	}
+
+	t := &UserProfile{
+		UserId:  m.UserId,
+		Name:    m.Name,
+		Content: m.Content,
+	}
+	res := conn.Model(NewUserProfile()).WithContext(ctx).Select("user_id", "name", "content").Where("user_id = ?", m.UserId).Updates(t)
+	if err := res.Error; err != nil {
+		return nil, err
+	}
+
+	return userProfile.SetUserProfile(t.UserId, t.Name, t.Content), nil
+}
+
+func (s *userProfileDao) UpdateList(ctx context.Context, tx *gorm.DB, ms userProfile.UserProfiles) (userProfile.UserProfiles, error) {
+	if len(ms) <= 0 {
+		return ms, nil
+	}
+
+	fms := ms[0]
+	for _, m := range ms {
+		if m.UserId != fms.UserId {
+			return nil, errors.NewError("userId is invalid")
+		}
+	}
+
+	var conn *gorm.DB
+	if tx != nil {
+		conn = tx
+	} else {
+		conn = s.ShardMysqlConn.Shards[keys.GetShardKeyByUserId(fms.UserId)].WriteMysqlConn
+	}
+
+	ts := NewUserProfiles()
+	for _, m := range ms {
+		t := &UserProfile{
+			UserId:  m.UserId,
+			Name:    m.Name,
+			Content: m.Content,
+		}
+		ts = append(ts, t)
+	}
+
+	res := conn.Model(NewUserProfile()).Clauses(clause.OnConflict{
+		Columns:   []clause.Column{{Name: "user_id"}},
+		DoUpdates: clause.AssignmentColumns([]string{"name", "content"}),
+	}).WithContext(ctx).Create(ts)
+	if err := res.Error; err != nil {
+		return nil, err
+	}
+
+	return ms, nil
+}
+
+func (s *userProfileDao) Delete(ctx context.Context, tx *gorm.DB, m *userProfile.UserProfile) error {
+	var conn *gorm.DB
+	if tx != nil {
+		conn = tx
+	} else {
+		conn = s.ShardMysqlConn.Shards[keys.GetShardKeyByUserId(m.UserId)].WriteMysqlConn
+	}
+
+	res := conn.Model(NewUserProfile()).WithContext(ctx).Where("user_id = ?", m.UserId).Delete(NewUserProfile())
+	if err := res.Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *userProfileDao) DeleteList(ctx context.Context, tx *gorm.DB, ms userProfile.UserProfiles) error {
+	if len(ms) <= 0 {
+		return nil
+	}
+
+	fms := ms[0]
+	for _, m := range ms {
+		if m.UserId != fms.UserId {
+			return errors.NewError("userId is invalid")
+		}
+	}
+
+	var conn *gorm.DB
+	if tx != nil {
+		conn = tx
+	} else {
+		conn = s.ShardMysqlConn.Shards[keys.GetShardKeyByUserId(fms.UserId)].WriteMysqlConn
+	}
+
+	var ks [][]interface{}
+	for _, m := range ms {
+		ks = append(ks, []interface{}{m.UserId})
+	}
+
+	res := conn.Model(NewUserProfile()).WithContext(ctx).Where("(user_id) IN ?", ks).Delete(NewUserProfile())
+	if err := res.Error; err != nil {
+		return err
+	}
+
+	return nil
+}
